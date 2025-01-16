e0b8a74297c7fa2ad7113a2e942bb664ad34afec
Rico <rico.suska@stud.hs-ruhrwest.de>
2025-01-16 22:29:11 +0100

    removed Test_client calls in Match_page

--- app/pages/match_page.tsx
+++ app/pages/match_page.tsx
@@ -41,11 +41,11 @@ export default function MatchOverviewPage() {
     const loadMatchData = async () => {
       try {
         console.log("Match page with Id", entityName);
-        const match = await fetchMatchTest(entityName);
+        const match = await fetchMatch(entityName);
         console.log("Match:", match);
         const hthMatches = await fetchHtHMatches(match.team1, match.team2);
-        const team1 = await fetchMatchRosterTest(match.matchId, match.team1);
-        const team2 = await fetchMatchRosterTest(match.matchId, match.team2);
+        const team1 = await fetchMatchRoster(match.matchId, match.team1);
+        const team2 = await fetchMatchRoster(match.matchId, match.team2);
         console.log(entityName);
         setMatch(match);
         console.log(team1);
