157e757debef7024a486436f0f671afa70de7b46
Rico Suska <rictotheo@t-online.de>
2025-01-19 21:10:04 +0100

    ngrok backup url

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,8 @@
 import { getAuthToken } from "../modules/auth/AuthProvider";
 
 //export const BASE_URL = "http://localhost:3000/api";
-export const BASE_URL = "http://localhost:3000/api";
+//export const BASE_URL = "https://fd35-2003-e5-df2a-a200-9d8f-db00-9f53-923.ngrok-free.app/api"
+export const BASE_URL = "http://192.168.178.21:3000/api";
 
 export async function apiFetchUnauthorized<T = unknown>(path: string, init?: RequestInit): Promise<T> {
     if (!path.startsWith("/")) throw new Error(`'${path} should start with '/'`);
